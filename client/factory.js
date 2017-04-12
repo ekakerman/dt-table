4134a6b9cfadad2314ee4ebfe6371234502ccae3
Elizabeth Akerman <ekakerman@gmail.com>
2017-04-12 02:44:39 -0400

    move data to factory

---
+++ client/factory.js
@@ -0,0 +1,86 @@
+angular.module('app.factory', [])
+
+.factory('Factory', function ($location) {
+
+    var data = [
+      {
+        ID: 1,
+        Address: '3728',
+        Street: 'Fishcreek Rd',
+        Side: 'Front',
+        Site: 1,
+        Species: 'Acer Rubrum',
+        DBH: 21,
+        Condition: 'Good'
+      },
+      {
+        ID: 2,
+        Address: '3728',
+        Street: 'Fishcreek Rd',
+        Side: 'Front',
+        Site: 2,
+        Species: 'Acer Rubrum',
+        DBH: 18,
+        Condition: 'Fair'
+      },
+      {
+        ID: 3,
+        Address: '3728',
+        Street: 'Fishcreek Rd',
+        Side: 'Front',
+        Site: 3,
+        Species: 'Acer Rubrum',
+        DBH: 19,
+        Condition: 'Poor'
+      },
+      {
+        ID: 4,
+        Address: '3728',
+        Street: 'Fishcreek Rd',
+        Side: 'Front',
+        Site: 3,
+        Species: 'Vacant Planting Site',
+        DBH: 0,
+        Condition: 'N/A'
+      },
+      {
+        ID: 5,
+        Address: '3728',
+        Street: 'Fishcreek Rd',
+        Side: 'Front',
+        Site: 4,
+        Species: 'Acer Rubrum',
+        DBH: 20,
+        Condition: 'Dead'
+      },
+      {
+        ID: 6,
+        Address: '3738',
+        Street: 'Fishcreek Rd',
+        Side: 'Front',
+        Site: 1,
+        Species: 'Vacant Planting Site',
+        DBH: 0,
+        Condition: 'N/A'
+      },
+      {
+        ID: 7,
+        Address: '3738',
+        Street: 'Fishcreek Rd',
+        Side: 'Front',
+        Site: 1,
+        Species: 'Fraxinus spp.',
+        DBH: 12,
+        Condition: 'Dead'
+      }
+    ];
+
+    var getData = function() {
+      return data;
+    }
+
+  return {
+    getData: getData
+  };
+
+});
--- client/table.js
+++ client/table.js
@@ -1,6 +1,6 @@
 angular.module('app.table', [])
 
-  .controller('tableCtrl', ['$scope', 'uiGridValidateService', '$window', function($scope, uiGridValidateService, $window){
+  .controller('tableCtrl', ['$scope', 'uiGridValidateService', '$window', 'Factory', function($scope, uiGridValidateService, $window, Factory){
 
     var checkVacancy = function(status) {
       var nonSpecies = ['VACANT', 'VACANT PLANTING SITE', ''];
@@ -126,78 +126,7 @@ angular.module('app.table', [])
       exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
     };
 
-    $scope.treeTable.data = [
-      {
-        ID: 1,
-        Address: '3728',
-        Street: 'Fishcreek Rd',
-        Side: 'Front',
-        Site: 1,
-        Species: 'Acer Rubrum',
-        DBH: 21,
-        Condition: 'Good'
-      },
-      {
-        ID: 2,
-        Address: '3728',
-        Street: 'Fishcreek Rd',
-        Side: 'Front',
-        Site: 2,
-        Species: 'Acer Rubrum',
-        DBH: 18,
-        Condition: 'Fair'
-      },
-      {
-        ID: 3,
-        Address: '3728',
-        Street: 'Fishcreek Rd',
-        Side: 'Front',
-        Site: 3,
-        Species: 'Acer Rubrum',
-        DBH: 19,
-        Condition: 'Poor'
-      },
-      {
-        ID: 4,
-        Address: '3728',
-        Street: 'Fishcreek Rd',
-        Side: 'Front',
-        Site: 3,
-        Species: 'Vacant Planting Site',
-        DBH: 0,
-        Condition: 'N/A'
-      },
-      {
-        ID: 5,
-        Address: '3728',
-        Street: 'Fishcreek Rd',
-        Side: 'Front',
-        Site: 4,
-        Species: 'Acer Rubrum',
-        DBH: 20,
-        Condition: 'Dead'
-      },
-      {
-        ID: 6,
-        Address: '3738',
-        Street: 'Fishcreek Rd',
-        Side: 'Front',
-        Site: 1,
-        Species: 'Vacant Planting Site',
-        DBH: 0,
-        Condition: 'N/A'
-      },
-      {
-        ID: 7,
-        Address: '3738',
-        Street: 'Fishcreek Rd',
-        Side: 'Front',
-        Site: 1,
-        Species: 'Fraxinus spp.',
-        DBH: 12,
-        Condition: 'Dead'
-      }
-    ];
+    $scope.treeTable.data = Factory.getData();
 
     $scope.treeTable.onRegisterApi = function(gridApi){
       $scope.gridApi = gridApi;

f1b8191303f5ff266b52a0f3a0aedaed08314128
Elizabeth Akerman <ekakerman@gmail.com>
2017-04-12 02:02:57 -0400

    remove comments

--- client/app.js
+++ client/app.js
@@ -9,14 +9,3 @@ angular.module('app', ['app.table', 'ngRoute', 'ngAnimate', 'ngTouch', 'ui.grid'
         redirectTo: '/'
       });
   });
-
-
-
-// var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.edit', 'ui.grid.cellNav', 'ui.grid.validate', 'addressFormatter']);
-
-// angular.module('addressFormatter', []).filter('address', function () {
-//   return function (input) {
-//       return input.street + ', ' + input.city + ', ' + input.state + ', ' + input.zip;
-//   };
-// });
-
